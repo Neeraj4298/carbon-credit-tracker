d027fc0b131b6a117215cb3e54e672f1925bebd5
kelothu.rahul <kelothu.rahul@tudip.com>
2025-04-21 14:51:12 +0530

    Serve frontend from Express

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const path = require('path');
 
 dotenv.config();
 const app = express();
@@ -17,11 +18,18 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 // Auth routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/trips', require('./routes/tripRoutes'));
 app.use('/api/employer', require('./routes/employerRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
+
 
 
 
